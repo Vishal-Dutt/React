0ecfc7af4744ea868dec3069a6f2b22b6229670a
Vishal <vishaldutt157@gmail.com>
2021-07-01 22:33:03 +0530

    Searching

--- React/movies/src/Components/Movies.js
+++ React/movies/src/Components/Movies.js
@@ -5,20 +5,42 @@ export default class Movies extends Component {
     constructor() {
         super();
         this.state = {
-            movies: getMovies()
+            movies: getMovies(),
+            currSearchText: ''
         }
     }
 
-    onDelete=(id)=>{
-        let arr = this.state.movies.filter(function(movieObj){
-            return movieObj._id!==id;
+    handleChange = (e) => {
+        let val = e.target.value;
+        // console.log(val);
+        this.setState({
+            currSearchText: val
+        })
+    }
+
+    onDelete = (id) => {
+        let arr = this.state.movies.filter(function (movieObj) {
+            return movieObj._id !== id;
         })
         // console.log(arr);
         this.setState({
-            movies:arr
+            movies: arr
         });
     }
+
     render() {
+        let { movies, currSearchText } = this.state;   // ES6 Destructuring
+        let filteredArr = [];
+        if (currSearchText === '') {
+            filteredArr = movies;
+        } else {
+            filteredArr = movies.filter(function (movieObj) {
+                let title = movieObj.title.toLowerCase();
+                // console.log(title);
+                return title.includes(currSearchText.toLowerCase());
+            })
+        }
+        // console.log("render");
         return (
             // JSX
             <div className='container'>
@@ -27,6 +49,7 @@ export default class Movies extends Component {
                         Hello
                     </div>
                     <div className='col-9'>
+                        <input type='search' value={this.state.currSearchText} onChange={this.handleChange}></input>
                         <table className="table">
                             <thead>
                                 <tr>
@@ -35,20 +58,20 @@ export default class Movies extends Component {
                                     <th scope="col">Genre</th>
                                     <th scope="col">Stock</th>
                                     <th scope="col">Rate</th>
-                                    <th scope="col"> </th>
+                                    <th></th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {
-                                    this.state.movies.map((movieObj) => {
+                                    filteredArr.map((movieObj) => {
                                         return (
-                                            <tr scope="row" ley={movieObj._id}>
+                                            <tr key={movieObj._id}>
                                                 <td></td>
                                                 <td>{movieObj.title}</td>
                                                 <td>{movieObj.genre.name}</td>
                                                 <td>{movieObj.numberInStock}</td>
                                                 <td>{movieObj.dailyRentalRate}</td>
-                                                <td><button onClick={function() {
+                                                <td><button onClick={function () {
                                                     this.onDelete(movieObj._id)
                                                 }.bind(this)} type="button" className="btn btn-danger">Delete</button></td>
                                             </tr>
