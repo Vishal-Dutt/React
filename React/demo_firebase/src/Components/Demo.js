eb9fad1f70a34cbcc469b9866f501f3c441aec65
Vishal <vishaldutt157@gmail.com>
2021-07-09 19:45:58 +0530

    Firebase SignIn

---
+++ React/demo_firebase/src/Components/Demo.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import firebase from './Firebase';
+
+function Demo() {
+    // console.log(firebase);
+    const auth = firebase.auth();
+    // Take eamil and password 
+    const [user, setUser] = useState(null);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async () => {
+        try {
+            console.log(email + "  " + password);
+            setError(true);
+            // predefined firebase funcion for authentication
+            let res = await auth.signInWithEmailAndPassword(email, password);
+            console.log(res.user);
+            setUser(res.user);
+            setLoading(false);
+        } catch (e){
+            // setError("Failed to Sign In");
+            setError(e.message);
+            setTimeout(() => {
+                setError('');
+            }, 2000)
+            setLoading(false);
+        }
+        setPassword('');
+        setEmail('');
+    }
+
+    const handleSignOut = () => {
+
+    }
+
+    return (
+        <>
+            {
+                // if loading then show loading if user is null the show login form 
+                loading ? <h1>Please Wait..... Loading</h1> : user === null ?
+                    <div>
+                        <label>
+                            Email:
+                            <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </label>
+                        <label>
+                            Password:
+                            <input type='text' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </label>
+                        <button onClick={handleSubmit}>Sign In </button>
+                        {/* if failed to loign show error otherwire show empty */}
+                        {error ? <h1>{error}</h1> : <></>}
+                    </div> :
+                    <>
+                        {/* if user is signed in then show which use is signed in and show sign out buttom */}
+                        <h2>{user.uid} is Signed In</h2>
+                        <button onClick={handleSignOut}>Sign Out</button>
+                    </>
+            }
+        </>
+    )
+}
+
+export default Demo
---
+++ React/demo_firebase/src/Components/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCzA5JqLfaZ7Je1UZNw-OxENAXOJA9eh1E",
+    authDomain: "fir-2c856.firebaseapp.com",
+    projectId: "fir-2c856",
+    storageBucket: "fir-2c856.appspot.com",
+    messagingSenderId: "1023524193957",
+    appId: "1:1023524193957:web:39778ea7b11edd8682ef4c"
+})
+
+export default firebase;
