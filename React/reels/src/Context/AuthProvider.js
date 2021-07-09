2d2b17db34b026e7b7ef196fb58b6b183bb22d0f
Vishal <vishaldutt157@gmail.com>
2021-07-10 01:14:09 +0530

    authContext

---
+++ React/reels/src/Context/AuthProvider.js
@@ -0,0 +1,56 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { aurth } from '..Firebase/'
+import { auth } from '../Firebase';
+
+// context
+const AuthContext = React.createContext();
+
+function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
+    function signup() {
+        return auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password);
+    }
+
+    function logout() {
+        return auth.signOut()
+    }
+
+    // componentDidMount
+    useEffect(() => {
+        // Adds an observer.
+        // componentDidMount
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+            setLoading(false);
+        })
+
+        // remove listner cleanup
+        // runs componentWillUnmount
+        return () => {
+            unsubscribe();
+        }
+
+    }, [])
+
+    const value = {
+        currentUser,
+        login,
+        signup,
+        logout
+    }
+
+    return (
+        <div>
+            <AuthContext.Provider value={value}>
+                {!loading && children}
+            </AuthContext.Provider>
+        </div>
+    )
+}
+
+export default AuthProvider
---
+++ React/reels/src/Firebase.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app'
+import "firebase/auth"
+import 'firebase/storage'
+import 'firebase/firestore'
+
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyBw8VwzlbgTHVTvAZdnM4U47f7mbkk9CGk",
+        authDomain: "reels-8c138.firebaseapp.com",
+        projectId: "reels-8c138",
+        storageBucket: "reels-8c138.appspot.com",
+        messagingSenderId: "761964607542",
+        appId: "1:761964607542:web:3f5f5d36cafada55378097"
+    }
+)
+export const auth = firebase.auth();
+const firestore = firebase.firestore();
+// Exporting/exposing only requried collections
+export const database = {
+    // To stroe data we have to create a collection to maake collection require collection object 
+    // 
+    users: firestore.collection('users'),
+
+    getCurrentTimeStamp: firebase.firestore.FieldValue.serverTimestamp  // current time 
+}
+
+export const storage = firebase.storage();
+// export default firebase;
